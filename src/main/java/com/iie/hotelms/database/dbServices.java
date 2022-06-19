8da07a059104b55d5397675cf1e13b3ddc83470c
Wojciech Pieczonka <wp113622@stud.ur.edu.plgit clone [200~https://gitlab.com/iie_projekt4/hotelmanagementsystem.git~git config --global user.email wp113622@stud.ur.edu.pl>
2022-06-19 18:55:34 +0200

    PROJ-40 <admin services management>

--- src/main/java/com/iie/hotelms/database/dbServices.java
+++ src/main/java/com/iie/hotelms/database/dbServices.java
@@ -1,4 +1,17 @@
 package com.iie.hotelms.database;
 
+import com.iie.hotelms.controllers.AdminWorkerslistScreen;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static com.iie.hotelms.database.DatabaseConnection.dbLink;
+
 public class dbServices {
+
+
+
 }
